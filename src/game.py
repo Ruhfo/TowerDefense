ed0adcf83ca7852e8b0fee1fd924b4474e76323a
ruhfo <ruhfo@localhost.localdomain>
2015-03-30 18:47:22 +0000

    Very first commit

---
+++ src/game.py
@@ -0,0 +1,20 @@
+import pygame
+pygame.init()
+
+#Create window 
+
+#Load images -> RAM
+#Load game data -> RAM
+
+#Generate Map from right corner if using isometric tiles
+
+#Start counting for waves
+while True:
+
+    #Game logic 
+
+    #Drawing
+
+    #Get userinput
+    #if gameEnd == True:
+       # quit()
